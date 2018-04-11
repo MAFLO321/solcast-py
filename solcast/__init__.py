85b5f7d7315c91aaeec8621d8c1507ab0076e923
Jared McCorkindale <jared.mccorkindale@anu.edu.au>
2018-04-11 12:56:25 +1000

    add in method to create multiple rooftop measurements

--- solcast/__init__.py
+++ solcast/__init__.py
@@ -8,6 +8,7 @@ from .radiation_forecasts import RadiationForecasts as get_radiation_forecasts
 from .radiation_estimated_actuals import RadiationEstimatedActuals as get_radiation_estimated_actuals
 from .rooftop_site import RooftopSite as post_rooftop_site
 from .rooftop_measurement import RooftopMeasurement as post_rooftop_measurement
+from .rooftop_measurements import RooftopMeasurements as post_rooftop_measurements
 
 from .pv_power_forecasts import PvPowerForecasts
 from .pv_power_estimated_actuals import PvPowerEstimatedActuals
@@ -15,4 +16,5 @@ from .radiation_forecasts import RadiationForecasts
 from .radiation_estimated_actuals import RadiationEstimatedActuals
 from .rooftop_site import RooftopSite
 from .rooftop_measurement import RooftopMeasurement
+from .rooftop_measurements import RooftopMeasurements
 
